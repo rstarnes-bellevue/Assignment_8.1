3162a7b56cf1564cf4c5942fb6e734e42e293a2b
Ryan <rstarnes@my365.bellevue.edu>
2021-07-29 19:56:35 -0500

    testing API key

--- Assignment_8.1.py
+++ Assignment_8.1.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import requests
+
+#def GetWeather (location):
+ #   try:
+  #      response = pro.openweathermap.org/data/2.5/forecast/hourly?zip=77706,US&appid=d56a5ad93c4f731841282b93a76c5a54
+
+
+
+
+
+
+
+print("Welcome to PyWeather! Please enter either a city or zip code for the current forecast.\n")
+
+#location = input("City/Zip code: ")
+
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast/hourly?zip=77706,US&appid=d56a5ad93c4f731841282b93a76c5a54")
+
+print(response.json())dfgdfg
