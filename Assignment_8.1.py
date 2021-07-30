e74ac3449cd9c8245672542cdece4c76ddddd5f1
Ryan <rstarnes@my365.bellevue.edu>
2021-07-29 20:05:33 -0500

    removed API key

--- Assignment_8.1.py
+++ Assignment_8.1.py
@@ -14,6 +14,6 @@ print("Welcome to PyWeather! Please enter either a city or zip code for the curr
 
 #location = input("City/Zip code: ")
 
-response = requests.get("https://api.openweathermap.org/data/2.5/forecast/hourly?zip=77706,US&appid=d56a5ad93c4f731841282b93a76c5a54")
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast/hourly?zip=77706,US&appid={key}")
 
 print(response.json())
